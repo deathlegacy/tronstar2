e99413d5aab4021b469b13cd6c9a5ebe25cf5eb9
deathlegacy <jessicaciscatan@yahoo.com>
2020-03-06 17:17:05 +0800

    Update messageReactionAdd.js

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -3,9 +3,9 @@ async function starEmbed(color, description, author, thumbnail, timestamp, foote
   const client = new Discord.Client();
     const embed = {
         "author": {
-            "name": `Ninja Hall Of Fame`,
-            "icon_url": "https://i.imgur.com/NSHqjRM.jpg",
-            "url": "https://ninja-bot.webnode.com/",
+            "name": `Hall Of Fame`,
+            "icon_url": "https://cdn.discordapp.com/attachments/92560678774120448/394428112164880384/TRON_MODIFIED.png",
+            "url": "https://cdn.discordapp.com/attachments/92560678774120448/394428112164880384/TRON_MODIFIED.png/",
         },
         "color": color,
         "description": "The following message has been starred",
@@ -35,13 +35,13 @@ const message = reaction.message;
 const config = require('../data/config.json');
 const embed2 = new Discord.RichEmbed()
 
-     embed2.setTitle("Ninja Starboard Channel")
+     embed2.setTitle("Starboard Channel")
       embed2.setColor(config.embedColor)
        embed2.setDescription("You do not have a ``ninja-starboard`` channel so i have created one for you and placed it at the top of the channel list, Move it where ever you wish")
         embed2.addField("Note", "To post the message you reacted to you will have to remove the reaction and do it again")
-        embed2.setFooter("© Ninja Starboard Bot")  
+        embed2.setFooter("Starboard Bot")  
 
-    const starboardChannel = reaction.message.guild.channels.find("name", "ninja-starboard");
+    const starboardChannel = reaction.message.guild.channels.find("name", "starboard");
          if (!message.guild.channels.exists("name", "ninja-starboard")) return reaction.message.channel.send(embed2) .then(message.guild.createChannel("ninja-starboard", "text"))       
          //if(!message.guild.channels.exists("name", "ninja-starboard")) return reaction.message.channel.send("Nope")
     try {
